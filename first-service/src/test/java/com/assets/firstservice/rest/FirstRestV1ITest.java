af479409e2851bb2c6b72f1a5f3935080a955f38
Roman Grupskyi <financeasist@gmail.com>
2022-12-08 15:36:53 +0200

    issue #4: One shared datasource (#10)
    * fixed common-dtos library
    
    * issue4: db and flyway migration changes
    - instead of separate db per service, each service communicates with one shared datasource
    - updated docker deployment
    - changed flyway migration to be in one place (first service).
    - separeted db migration and initialization with test data.
    
    * issue4: db and flyway migration changes
    - fixed sequences
    - updated init sql script
    
    Co-authored-by: roman <rhrupskyi@exoft.net>

--- first-service/src/test/java/com/assets/firstservice/rest/FirstRestV1ITest.java
+++ first-service/src/test/java/com/assets/firstservice/rest/FirstRestV1ITest.java
@@ -1,6 +1,5 @@
 package com.assets.firstservice.rest;
 
-
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
